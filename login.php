60528230a9c6e6745f4ff871b53df057f48b2052
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-06-30 17:21:58 -0400

    alert-danger in errorsql

--- login.php
+++ login.php
@@ -25,7 +25,7 @@
                                     </button>
                                 </div>';
                     }if($cod_error == "sqlerror"){
-                        echo '  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        echo '  <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                     <strong>Error del sistema!</strong>
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
