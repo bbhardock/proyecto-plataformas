bd8c2f0d88dbd118f69429d699891882e66b6263
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-07-10 19:16:55 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <footer class="footer ">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-6">
             <img class="logo" src="static/images/medicinaucn.png" alt="">
             <img class="acreditacion" src="static/images/acreditacion.png" alt="">
-        </div>   
+        </div>         
+        <div class="contacto col-md-6">            
+            <h6>Actividades relacionadas a la facultad de medicina</h6>
+            <h6><strong>Contactos Sede Coquimbo:</strong></h6>
+        </div>
         <div class="col-md-6">
             <h6><strong>Vinculación con el medio:</strong></h6>
             <h6>Universidad Católica del Norte</h6>
             <h6>Fono (+56) 9 9815 6756</h6>
         </div>    
-        <div class="col-md-6">            
-            <h6>Actividades relacionadas a la facultad de medicina</h6>
-            <h6><strong>Contactos Sede Coquimbo:</strong></h6>
-        </div>
     </div>
 </footer> 
