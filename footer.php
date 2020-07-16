5ac3b5a98c4dbeec1ae227c40561333ed2edde84
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-07-15 22:04:05 -0400

    cambios al footer
    Quedo reposicionado y con cambio de tamaño

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
 <footer class="footer">
     <div class="row">
-        <div class="col-sm-12 col-md-6">
+        <div class="col-sm-12 col-md-12 col-lg-6">
             <img class="acreditacion" src="static/images/medi_acreditacion.png" alt="">
-        </div>         
-        <div class="contacto col-sm-12 col-md-6">            
+        </div>            
+        <div class="contacto col-md-12 col-lg-4">  
+            <h5><strong>Vinculación con el Medio:</strong></h5>
             <h6>Actividades relacionadas a la Facultad de Medicina (FACMED)</h6>
-            <h6><strong>Contactos Sede Coquimbo:</strong></h6>
-        </div>   
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <h6><strong>Vinculación con el Medio:</strong></h6>
             <h6>Universidad Católica del Norte</h6>
-            <h6>Fono (+56) 9 9815 6756</h6>
+        </div>       
+        <div class="contacto col-md-12 col-lg-2">            
+            <h5><strong>Contactos Sede Coquimbo:</strong></h5>
+            <h6>Correo: vinculacionfamed@ucn.cl</h6>
+            <h6>Fono: (51) 22 05 86</h6>
         </div> 
     </div>
 </footer> 
