ad4df81674ec12f9d2b344f2b183a25bd80ef1c3
iRodHG <49417856+iRodHG@users.noreply.github.com>
2020-08-20 15:50:33 -0400

    Funcion Excel 0.1
    La funcion reporte funciona, pero todavia esta en un formato muy basico

--- includes/reporte.inc.php
+++ includes/reporte.inc.php
@@ -1,6 +1,12 @@
 <?php
 if(isset($_POST['reporte-submit'])){
-    echo "Aqui esta";
+    require 'SimpleXLSXGen.php';
+    $SimpleXLSXGen = new SimpleXLSXGen();
+    $data = [
+        ['Codigo','Responsable','Nombre Actividad','Fecha Ejecucion'],
+        ['Datos1','Datos2','Datos3','Datos4'],
+    ];
+    SimpleXLSXGen::fromArray( $data )->downloadAs('Reporte.xlsx');
 }else{
     header("Location ../dashboard.php");
     exit();
