9305b62b31bf48da491b04d752aadd23e3ed3cd2
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-08-21 16:28:13 -0400

    arreglo bug formulario

--- index.php
+++ index.php
@@ -533,9 +533,7 @@
                         { name: 'Chrome', y: 61.41 },
                         { name: 'Internet Explorer', y: 11.84 },
                         { name: 'Firefox', y: 10.85 },
-                        { name: 'Edge', y: 4.67 },
-                        { name: 'Safari', y: 4.18 },
-                        { name: 'Other', y: 7.05 }
+                        { name: 'Edge', y: 4.67 }
                     ]
                 }]
             });
