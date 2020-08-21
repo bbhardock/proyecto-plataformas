ac79910da95c8429f6a7844187f13e30604aab49
bbhardock <50248662+bbhardock@users.noreply.github.com>
2020-08-21 16:38:57 -0400

    Quitados los echo de una linea, reemplazados por multilinea

--- header.php
+++ header.php
@@ -13,12 +13,12 @@
                         echo '<li><a href="/login.php">Iniciar Sesión</a></li>';
                     }
                     else if($_SESSION['user_admin_status']=='S'){
-                        echo '<li><a href="/dashboard.php">Todas las actividades</a></li>';
-                        echo '<li><a href="/adminUser.php">Administración de usuarios</a></li>';
-                        echo '<li><a href="/includes/logout.inc.php">Cerrar Sesión</a></li>';
+                        echo '<li><a href="/dashboard.php">Todas las actividades</a></li>
+                            <li><a href="/adminUser.php">Administración de usuarios</a></li>
+                            <li><a href="/includes/logout.inc.php">Cerrar Sesión</a></li>';
                     }else{
-                        echo '<li><a href="/dashboard.php">Mis Actividades</a></li>';
-                        echo '<li><a href="/includes/logout.inc.php">Cerrar Sesión</a></li>';
+                        echo '<li><a href="/dashboard.php">Mis Actividades</a></li>
+                            <li><a href="/includes/logout.inc.php">Cerrar Sesión</a></li>';
                     }
                 ?>
             </ul>
