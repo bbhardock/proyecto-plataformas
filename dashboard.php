961a9d77a8f4bd825ebaa7ee09e3e5ad884057c7
NicoSv19 <39229122+NicoSv19@users.noreply.github.com>
2020-08-28 12:05:22 -0400

    arreglo de tamaño cuando se achica la pantalla de resumen

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@
         <meta name="viewport" content="width = device-width, user-scalable = no">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="static/css/fontello.css?v1.6">
-        <link rel="stylesheet" href="static/css/Styles.css?v3.18">
+        <link rel="stylesheet" href="static/css/Styles.css?v4.0">
         
         <!-- Fuente -->
         <link rel="stylesheet" href="https://use.typekit.net/jyw0mhj.css">
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <meta name="viewport" content="width = device-width, user-scalable = no">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="static/css/fontello.css?v1.8"/>
-        <link rel="stylesheet" href="static/css/styleIndex.css?v3.8"/>
+        <link rel="stylesheet" href="static/css/styleIndex.css?v3.9"/>
         <!-- Fuente -->
         <link rel="stylesheet" href="https://use.typekit.net/jyw0mhj.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400&display=swap">
